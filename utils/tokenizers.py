e24fac2ae1e636243222d3ec2452315f1d5acabe
vinhdq842 <quangvinh0842@gmail.com>
2024-03-07 10:03:32 +0700

    make the new BPETokenizer usable

--- utils/tokenizers.py
+++ utils/tokenizers.py
@@ -9,53 +9,59 @@ from tqdm import tqdm
 class Tokenizer:
     r"""Base class for tokenizers."""
 
-    pad = "<pad>"
-    sos = "<sos>"
-    eos = "<eos>"
-    unk = "<unk>"
+    PAD = "<pad>"
+    SOS = "<sos>"
+    EOS = "<eos>"
+    UNK = "<unk>"
 
     def __init__(self):
         self.special_tokens = {
-            Tokenizer.pad: 0,
-            Tokenizer.sos: 1,
-            Tokenizer.eos: 2,
-            Tokenizer.unk: 3,
+            Tokenizer.PAD: 0,
+            Tokenizer.SOS: 1,
+            Tokenizer.EOS: 2,
+            Tokenizer.UNK: 3,
         }
         self.inverse_special_tokens = {v: k for k, v in self.special_tokens.items()}
         self.vocab = {}
 
     def register_special_tokens(self, special_tokens: Dict[str, int]):
+        r"""Add new special tokens."""
         self.special_tokens.update(special_tokens)
         self.inverse_special_tokens = {v: k for k, v in self.special_tokens.items()}
 
-    def build(self, raw_corpus: List[str], target_size=None):
+    def build(
+        self, raw_corpus: List[str], target_size: int = 256, verbose: bool = False
+    ):
+        r"""Train the tokenizer on the given corpus."""
         raise NotImplementedError
 
-    def encode(self, input: str):
+    def encode(self, text: str) -> List[int]:
+        r"""Encode a trivial text."""
         raise NotImplementedError
 
-    def decode(self, input: List[int]):
+    def decode(self, ids: List[int]) -> str:
+        r"""Decode a list of token ids."""
         raise NotImplementedError
 
-    def state_dict(self):
+    def state_dict(self) -> Dict[str, Any]:
         raise NotImplementedError
 
     def load_state_dict(self, state_dict: Dict[str, Any]):
         raise NotImplementedError
 
     def __call__(
-        self, input: Union[str, Iterable[str]]
+        self, text: Union[str, Iterable[str]]
     ) -> Union[List[int], List[List[str]]]:
-        if single := isinstance(input, str):
-            input = [input]
+        if single := isinstance(text, str):
+            text = [text]
 
-        res = []
-        for inp in input:
-            res.append(self.encode(inp))
+        ids = []
+        for c in text:
+            ids.append(self.encode(c))
 
-        return res[0] if single else res
+        return ids[0] if single else ids
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.vocab) + len(self.special_tokens)
 
 
@@ -73,43 +79,58 @@ class BPETokenizer(Tokenizer):
         self.merges = {}
         self.inverse_vocab = {}
 
-    def _compute_pair_freqs(self, text: List[str], freqs=None):
-        if freqs is None:
-            freqs = defaultdict(int)
+    def _merge_pair(
+        self, pair: Tuple[str, str], splits: Dict[str, List[str]]
+    ) -> Dict[str, List[str]]:
+        r"""Reflect the merge onto the splits."""
+        for chunk, split in splits.items():
+            i = 0
+            while i < len(split) - 1:
+                if (split[i], split[i + 1]) == pair:
+                    split = split[:i] + [split[i] + split[i + 1]] + split[i + 2 :]
+                else:
+                    i += 1
+            splits[chunk] = split
+        return splits
 
-        for p1, p2 in zip(text, text[1:]):
-            freqs[(p1, p2)] += 1
+    def _compute_pair_freqs(
+        self, chunk_freqs: Dict[str, int], splits: Dict[str, List[str]]
+    ) -> Dict[str, int]:
+        r"""Compute pair frequencies."""
+        pair_freqs = defaultdict(int)
+        for chunk, freq in chunk_freqs.items():
+            split = splits[chunk]
+            for p1, p2 in zip(split, split[1:]):
+                pair_freqs[(p1, p2)] += freq
 
-        return freqs
+        return pair_freqs
 
-    def _pretokenize(self, raw_text: str) -> List[List[str]]:
+    def _prepair(self, raw_text: str) -> Tuple[Dict[str, int], Dict[str, List[str]]]:
         r"""Split the text into separated chunks and turn each chunk into characters."""
-        res = []
-        for text in re.findall(self.split_pattern, raw_text):
-            if self.lower:
-                text = text.lower()
+        chunk_freqs = defaultdict(int)
+        splits = {}
+        for chunk in re.findall(self.split_pattern, raw_text):
+            chunk_freqs[chunk] += 1
 
-            res.append(list(text))
+            if chunk in splits:
+                continue
 
-        return res
+            splits[chunk] = list(chunk)
 
-    def _merge_pair(self, pair: Tuple[str, str], chars: List[str]):
-        i = 0
-        while i < len(chars) - 1:
-            if (chars[i], chars[i + 1]) == pair:
-                chars = chars[:i] + [chars[i] + chars[i + 1]] + chars[i + 2 :]
-            else:
-                i += 1
+        return chunk_freqs, splits
 
-        return chars
-
-    def build(self, raw_corpus: List[str], target_size: int, verbose: bool = False):
+    def build(
+        self, raw_corpus: List[str], target_size: int = 256, verbose: bool = False
+    ):
         assert target_size > len(self)
 
         raw_text = "\n".join(raw_corpus)
-        corpus = self._pretokenize(raw_text)
+        if self.lower:
+            raw_text = raw_text.lower()
 
-        for c in list(set(raw_text))[:target_size]:
+        chunk_freqs, splits = self._prepair(raw_text)
+
+        for c in list(set("".join(chunk_freqs.keys())))[:target_size]:
             self.vocab[len(self)] = c
 
         pbar = tqdm(
@@ -120,15 +141,13 @@ class BPETokenizer(Tokenizer):
         )
 
         while len(self) < target_size:
-            pair_freqs = defaultdict(int)
-            for chunk in corpus:
-                self._compute_pair_freqs(chunk, pair_freqs)
+            pair_freqs = self._compute_pair_freqs(chunk_freqs, splits)
+
             if len(pair_freqs):
                 best_pair = max(pair_freqs, key=pair_freqs.get)
-
                 self.merges[best_pair] = len(self)
                 self.vocab[len(self)] = best_pair[0] + best_pair[1]
-                corpus = [self._merge_pair(best_pair, p) for p in corpus]
+                splits = self._merge_pair(best_pair, splits)
                 pbar.update(1)
             else:
                 warnings.warn("Not enough pair to merge. Stopping...")
@@ -137,17 +156,29 @@ class BPETokenizer(Tokenizer):
         self.inverse_vocab = {v: k for k, v in self.vocab.items()}
 
     def _encode_chunk(self, chars: List[str]) -> List[int]:
+        r"""Do the actual encoding."""
         while len(chars) >= 2:
-            stats = self._compute_pair_freqs(chars)
-            pair = min(stats, key=lambda p: self.merges.get(p, float("inf")))
+            stats = defaultdict(int)
+            for p1, p2 in zip(chars, chars[1:]):
+                stats[(p1, p2)] += 1
 
+            pair = min(stats, key=lambda p: self.merges.get(p, float("inf")))
             if pair not in self.merges:
                 break
-            chars = self._merge_pair(pair, chars)
 
-        return [self.inverse_vocab.get(c, self.special_tokens[self.unk]) for c in chars]
+            idx = 0
+            while idx < len(chars) - 1:
+                if (chars[idx], chars[idx + 1]) == pair:
+                    chars = (
+                        chars[:idx] + [chars[idx] + chars[idx + 1]] + chars[idx + 2 :]
+                    )
+                else:
+                    idx += 1
 
-    def _encode(self, text: str) -> List[int]:
+        return [self.inverse_vocab.get(c, self.special_tokens[self.UNK]) for c in chars]
+
+    def _encode_ordinary(self, text: str) -> List[int]:
+        r"""Encode a text chunk that is not supposed to contain special tokens."""
         chunks = re.findall(self.split_pattern, text)
         ids = []
         for chunk in chunks:
@@ -156,6 +187,9 @@ class BPETokenizer(Tokenizer):
         return ids
 
     def encode(self, text: str) -> List[int]:
+        if self.lower:
+            text = text.lower()
+
         special_pattern = (
             "(" + "|".join(re.escape(s) for s in self.special_tokens) + ")"
         )
@@ -166,24 +200,25 @@ class BPETokenizer(Tokenizer):
             if chunk in self.special_tokens:
                 ids.append(self.special_tokens[chunk])
             else:
-                ids.extend(self._encode(chunk))
+                ids.extend(self._encode_ordinary(chunk))
 
         return ids
 
-    def decode(self, input: List[int]) -> str:
+    def decode(self, ids: List[int]) -> str:
         tokens = []
-        for token in input:
-            if token in self.inverse_special_tokens:
-                tokens.append(self.inverse_special_tokens[token])
-            elif token in self.vocab:
-                tokens.append(self.vocab[token])
+        for id in ids:
+            if id in self.inverse_special_tokens:
+                tokens.append(self.inverse_special_tokens[id])
+            elif id in self.vocab:
+                tokens.append(self.vocab[id])
             else:
-                raise ValueError(f"Invalid token id: {token}")
+                raise ValueError(f"Invalid token id: {id}")
 
         return "".join(tokens)
 
-    def state_dict(self):
+    def state_dict(self) -> Dict[str, Any]:
         return {
+            "class_name": self.__class__.__name__,
             "merges": self.merges,
             "vocab": self.vocab,
             "lower": self.lower,
@@ -205,18 +240,21 @@ class BPETokenizer(Tokenizer):
 class ByteLevelBPETokenizer(Tokenizer):
     r"""Byte-level Byte-Pair Encoding Tokenizer for shared vocabulary."""
 
-    def __init__(self): ...
+    def __init__(self):
+        super().__init__()
 
-    def build(self, raw_corpus: List[str], target_size=None):
+    def build(
+        self, raw_corpus: List[str], target_size: int = 256, verbose: bool = False
+    ):
         raise NotImplementedError
 
-    def encode(self, input: Union[List[List[str]], List[str]]):
+    def encode(self, text: str) -> List[int]:
         raise NotImplementedError
 
-    def decode(self, input: Union[List[List[int]], List[int]]):
+    def decode(self, ids: List[int]) -> str:
         raise NotImplementedError
 
-    def state_dict(self):
+    def state_dict(self) -> Dict[str, Any]:
         raise NotImplementedError
 
     def load_state_dict(self, state_dict: Dict[str, Any]):
